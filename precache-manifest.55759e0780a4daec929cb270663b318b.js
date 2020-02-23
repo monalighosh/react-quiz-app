d7eaa252c8388fe3db13fdfb7d4d8217db3bf510
Monali Ghosh <monalighosh@users.noreply.github.com>
2020-02-23 22:57:43 +1100

    Updates

---
+++ precache-manifest.55759e0780a4daec929cb270663b318b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8b37872eb9aa88ab785e2410c3ded9",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "edf57d631b335752e8cc",
+    "url": "/react-quiz-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "000fdd4816ab4c3d127c",
+    "url": "/react-quiz-app/static/js/2.8bc6f351.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-quiz-app/static/js/2.8bc6f351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf57d631b335752e8cc",
+    "url": "/react-quiz-app/static/js/main.e78453f4.chunk.js"
+  },
+  {
+    "revision": "958007575758f51619d9",
+    "url": "/react-quiz-app/static/js/runtime-main.2be7e22d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-quiz-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
